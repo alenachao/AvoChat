c835259363a424e0e87768ca3e903ee8c987f935
Alena Chao <122919697+alenachao@users.noreply.github.com>
2024-05-14 23:14:49 -0700

    deleted hugging face references

--- model.py
+++ model.py
@@ -1,20 +1,14 @@
 # the os module helps us access environment variables
 # i.e., our API keys
 import os
-
-# these modules are for querying the Hugging Face model
 import json
 from dotenv import load_dotenv
 import requests
-
-# the Discord Python API
 import discord
 
 # load environment variables
 load_dotenv()
-HUGGINGFACE_TOKEN=os.environ['HUGGINGFACE_TOKEN']
-DISCORD_TOKEN=os.environ['HUGGINGFACE_TOKEN']
-
+DISCORD_TOKEN=os.environ["DISCORD_TOKEN"]
 
 from transformers import AutoModelWithLMHead, AutoModelForCausalLM, AutoTokenizer
 import torch
@@ -23,26 +17,16 @@ import torch
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 model = AutoModelWithLMHead.from_pretrained("microsoft/DialoGPT-small")
 
-# this is my Hugging Face profile link
-API_URL = 'https://api-inference.huggingface.co/models/alenachao/'
-
 class MyClient(discord.Client):
     def __init__(self, model_name):
         # adding intents module to prevent intents error in __init__ method in newer versions of Discord.py
         intents = discord.Intents.default() # Select all the intents in your bot settings as it's easier
         intents.message_content = True
         super().__init__(intents=intents)
-        self.api_endpoint = API_URL + model_name
-        # retrieve the secret API token from the system environment
-        huggingface_token = HUGGINGFACE_TOKEN
-        # format the header in our request to Hugging Face
-        self.request_headers = {
-            'Authorization': 'Bearer {}'.format(huggingface_token)
-        }
 
     def query(self, payload):
         """
-        make request to the Hugging Face model API
+        query model
         """
         data = json.dumps(payload)
         data = json.loads(data)
